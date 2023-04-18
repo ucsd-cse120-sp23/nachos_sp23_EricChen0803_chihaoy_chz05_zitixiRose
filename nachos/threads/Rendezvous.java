8eaaa0993bbe5e6def79e8b73cf725f581a13d74
chz05 <zhouchenyang2011@gmail.com>
2023-04-18 16:54:33 -0700

    Finish project 1 - task 5

--- nachos/threads/Rendezvous.java
+++ nachos/threads/Rendezvous.java
@@ -1,6 +1,7 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import java.util.*;
 
 /**
  * A <i>Rendezvous</i> allows threads to synchronously exchange values.
@@ -9,7 +10,13 @@ public class Rendezvous {
     /**
      * Allocate a new Rendezvous.
      */
+    private Lock lock;
+    private Map<Integer, Condition> tagToCV;
+    private Map<Integer, Integer> tagToValue;
     public Rendezvous () {
+        lock = new Lock();
+        tagToCV = new HashMap<Integer, Condition>();
+        tagToValue = new HashMap<Integer, Integer>();
     }
 
     /**
@@ -29,6 +36,71 @@ public class Rendezvous {
      * @param value the integer to exchange.
      */
     public int exchange (int tag, int value) {
-	return 0;
+        lock.acquire();
+        if (!tagToCV.containsKey(tag)){
+            System.out.println(KThread.currentThread().getName() + tag + " " + value);
+            Condition cv = new Condition(lock);
+            tagToCV.put(tag, cv);
+            tagToValue.put(tag, value);
+            cv.sleep();
+            System.out.println("Ok, I wake up.");
+            int exchangeValue = tagToValue.get(tag);
+            tagToCV.remove(tag);
+            tagToValue.remove(tag);
+            lock.release();
+            return exchangeValue;
+        }
+        else {
+            System.out.println(KThread.currentThread().getName() + tag + " " + value);
+            Condition cv = tagToCV.get(tag);
+            int exchangevalue = tagToValue.get(tag);
+            tagToValue.put(tag, value);
+            cv.wake();     
+            lock.release();       
+            return exchangevalue;
+        }
     }
+
+    public static void rendezTest1() {
+	final Rendezvous r = new Rendezvous();
+
+	KThread t1 = new KThread( new Runnable () {
+		public void run() {
+		    int tag = 0;
+		    int send = -1;
+
+		    System.out.println ("Thread " + KThread.currentThread().getName() + " exchanging " + send);
+            System.out.println("Can not work here.");
+		    int recv = r.exchange (tag, send);
+            System.out.println(recv);
+		    Lib.assertTrue (recv == 1, "Was expecting " + 1 + " but received " + recv);
+		    System.out.println ("Thread " + KThread.currentThread().getName() + " received " + recv);
+		}
+	    });
+	t1.setName("t1");
+	KThread t2 = new KThread( new Runnable () {
+		public void run() {
+		    int tag = 0;
+		    int send = 1;
+
+		    System.out.println ("Thread " + KThread.currentThread().getName() + " exchanging " + send);
+		    int recv = r.exchange (tag, send);
+		    Lib.assertTrue (recv == -1, "Was expecting " + -1 + " but received " + recv);
+		    System.out.println ("Thread " + KThread.currentThread().getName() + " received " + recv);
+		}
+	    });
+	t2.setName("t2");
+
+	t1.fork(); t2.fork();
+	// assumes join is implemented correctly
+	t1.join(); t2.join();
+    }
+
+    // Invoke Rendezvous.selfTest() from ThreadedKernel.selfTest()
+
+    public static void selfTest() {
+	// place calls to your Rendezvous tests that you implement here
+	rendezTest1();
+    }
+
 }
--- nachos/threads/ThreadedKernel.java
+++ nachos/threads/ThreadedKernel.java
@@ -46,6 +46,7 @@ public class ThreadedKernel extends Kernel {
 	 * here.
 	 */
 	public void selfTest() {
+		Rendezvous.selfTest();
 		Condition2.selfTest();
 		KThread.selfTest();
 		Semaphore.selfTest();
